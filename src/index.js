c1072cc904d6251aba6278913ce3fe999fd079fe
lucas couto <tesye30@hotmail.com>
2021-09-23 23:59:26 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ const { v4 : uuidv4 } = require("uuid")
 const app = express();
 app.use(express.json());
 
+
+
 const customers = [];
+
+// Middleware
+function verifyIfExistsAccontCPF(request, response, next){
+    const { cpf} = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+    return next();
+}
 
 
 /**
@@ -43,14 +57,8 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-    const { cpf} = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer) {
-        return response.status(400).json({ error: "Customer not found"});
-    }
+app.get("/statement", verifyIfExistsAccontCPF,(request, response) => {
+    
 
     return response.json(customer.statement);
 });
