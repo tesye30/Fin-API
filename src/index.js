9262bb0ee49e55073ba0967f3cadebad92f3af95
lucas couto <tesye30@hotmail.com>
2021-09-23 23:52:48 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
-const { response } = require('express');
+
+const { request, response } = require('express');
 const express = require('express');
+const { v4 : uuidv4 } = require("uuid")
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.send("Teste  dsadasdasdas");
+const customers = [];
+
+
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação especifica
+ * DELETE - Deletar uma informação no servidor 
+ */
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+        );
+
+        if(customerAlreadyExists) {
+            return response.status(400).json({ error: "Customer already exists !"});
+        }
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    });
+    return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+    const { cpf} = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(8080);
